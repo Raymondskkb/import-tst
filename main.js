4ae649a6ac6aeb8fc753ad9a82f285aaa5f40db3
Raymond Sekikubo <raymondskkb@gmail.com>
2022-03-13 00:02:55 +0300

    push test

---
+++ main.js
@@ -0,0 +1,7 @@
+// document.getElementById("label").textContent = add(2,10)
+// // document.getElementById("label").textContent = 3
+// let clear = document.getElementById("label").clearContent
+// document.getElementById("bttn-clear").textContent = clear
+
+
+//document.getElementById("label").innerText = add(num1,num2);
